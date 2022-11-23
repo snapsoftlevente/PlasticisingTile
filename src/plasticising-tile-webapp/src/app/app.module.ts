cfc1079607c8eee72e4689eceee01ac5b3fa8e8a
Levente Barna <levente.barna@snapsoft.hu>
2022-11-23 14:10:50 +0100

    add search input to column list

--- src/plasticising-tile-webapp/src/app/app.module.ts
+++ src/plasticising-tile-webapp/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header/header.component';
@@ -39,6 +40,7 @@ import { ConfigurationChartComponent } from './tile-configuration/configuration-
     MatDividerModule,
     MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatNativeDateModule,
     MatToolbarModule,
   ],
