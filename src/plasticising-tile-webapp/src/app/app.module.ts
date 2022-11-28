5cbc63fd567d002e823cf0a1ede91822fa30ed9a
Levente Barna <levente.barna@snapsoft.hu>
2022-11-28 11:40:18 +0100

    configure api root url by enviroment variable (instead of being hard-wired)

--- src/plasticising-tile-webapp/src/app/app.module.ts
+++ src/plasticising-tile-webapp/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ApiModule } from '@api/api.module';
 import { CoreModule } from '@core/core.module';
 import { SharedModule } from '@shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
+
+import { environment } from '@env';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { HeaderComponent } from './layout/header/header.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    ApiModule.forRoot({ rootUrl: environment.apiRootUrl }),
     CoreModule,
     SharedModule
   ],
