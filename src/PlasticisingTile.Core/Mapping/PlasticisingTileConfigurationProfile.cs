b9bb96f54f35248fffbbd7ff341e480a8d584f4a
Levente Barna <levente.barna@snapsoft.hu>
2022-11-28 15:50:06 +0100

    skip timestamp column when fetching available columns

--- src/PlasticisingTile.Core/Mapping/PlasticisingTileConfigurationProfile.cs
+++ src/PlasticisingTile.Core/Mapping/PlasticisingTileConfigurationProfile.cs
@@ -8,6 +8,8 @@ public class PlasticisingTileConfigurationProfile : Profile
     public PlasticisingTileConfigurationProfile()
     {
         CreateMap<DatasourceBo, PlasticisingTileConfigurationBo>()
-            .ForMember(dest => dest.AvailableColumns, opt => opt.MapFrom(src => src.DatasourceColumns));
+            .ForMember(
+                dest => dest.AvailableColumns,
+                opt => opt.MapFrom(src => src.DatasourceColumns.Where(dsc => !string.IsNullOrEmpty(dsc.Key) && !dsc.Key.Equals(src.TimestampColumnName))));
     }
 }
